a753454fcee01afec362f9c6bc162c5a2916f6b5
Chris McIntosh <Chocrates@github.com>
2021-02-04 15:59:14 -0600

    First commit

---
+++ TestConsoleTests/AddTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole.Tests
+{
+    [TestClass()]
+    public class AddTests
+    {
+        [TestMethod()]
+        public void addTest()
+        {
+            Add add = new Add();
+            Assert.AreEqual(4, add.add(1, 3));
+        }
+    }
+}
